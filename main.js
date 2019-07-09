089d221caccd2a47d4e1fc37b8021c0efb534559
luna <1661639144@qq.com>
2019-07-09 09:39:20 +0800

    xiugaijs

---
+++ main.js
@@ -0,0 +1,16 @@
+var user=new Object;
+user.name='John';
+user.surname='Mike';
+user.name='Peter';
+user.name='';
+console.log(user)
+
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+// write your code here...
+var  count=fruit.apple+fruit.peach+fruit.pear;
+alert(count);
